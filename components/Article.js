9e3b0a74b8b3ccd8c37f6b379b8ccc8650ee9b67
golsa <golsaabdolahi73@gmail.com>
2023-03-13 10:56:48 +0330

    userouter in article component

--- components/Article.js
+++ components/Article.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Box, Typography} from "@mui/material";
-import Link from "next/link";
+// import Link from "next/link";
 import moment from "moment";
-
+import { useRouter } from 'next/router'
 
 const Article = ({item}) => {
     const url=item.title.replaceAll(" ","-")
+    const router = useRouter()
 
     const wpm = 225;
     const words = item.body.trim().split(/\s+/).length;
@@ -14,14 +15,17 @@ const Article = ({item}) => {
     return (
         <article style={{marginTop:'2.5rem',marginBottom:'2.5rem'}}>
             <Box>
-                <Typography variant={"h3"} color="text.secondary" mb={'0.5rem'}>
-                    <Link  href={{
-                        pathname: `/${url}`,
-                        query: {id:item.id},
-                    }}
+                <Typography variant={"h3"} color="text.secondary" mb={'0.5rem'} sx={{cursor:'pointer'}}>
+                    <span  onClick={()=>
+                    {
+                        router.push({
+                            pathname: `/${url}`,
+                            query: {id:item.id,date:item.date}},`/${url}`)
+                    }
+                    }
                      style={{color:'inherit',textDecoration:'none'}}>
                         {item.title}
-                    </Link>
+                    </span>
                 </Typography>
                 <small style={{fontSize:'80%'}}>
                     {`${moment(item.date).format('LL')} • ️☕️ ${time} min read`}
